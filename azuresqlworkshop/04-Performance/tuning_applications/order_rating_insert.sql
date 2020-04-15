b430c267ab98b2dcc9b9d7d8b33d759a6664b7ea
Bob Ward <bobward@microsoft.com>
2020-04-15 14:38:56 -0500

    Fix back activity 3 scripts until ready
    Revered accidental changes here until ready to modify

--- azuresqlworkshop/04-Performance/tuning_applications/order_rating_insert.sql
+++ azuresqlworkshop/04-Performance/tuning_applications/order_rating_insert.sql
@@ -1,5 +1,6 @@
 DECLARE @x int;
 SET @x = 0;
+BEGIN TRAN;
 WHILE (@x < 100)
 BEGIN
 SET @x = @x + 1;
@@ -7,4 +8,5 @@ INSERT INTO SalesLT.OrderRating
 (SalesOrderID, OrderRatingDT, OrderRating, OrderRatingComments)
 VALUES (@x, getdate(), 5, 'This was a great order');
 END
+COMMIT TRAN;
 GO
